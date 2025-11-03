b53b7c9be111608c239ad811a6453ab1743f500c
LenaFedchenko <lenafedcenko98@gmail.com>
2025-11-03 21:04:31 +0200

    added comix basic

---
+++ Assets/Scripts/HideImage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+public class HideImage : MonoBehaviour
+{
+    public Image introImage;
+    public float displayTime = 3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (introImage != null)
+            Invoke("Hide", displayTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Hide()
+    {
+        introImage.gameObject.SetActive(false);
+    }
+}
